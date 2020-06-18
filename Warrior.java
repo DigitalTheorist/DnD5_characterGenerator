929aa4ea075ba17204ca194e324d6d600f3fc058
jcostopulos <costopulos@gmail.com>
2020-06-17 19:41:26 -0700

    add Warrior class as seperate file

---
+++ Warrior.java
@@ -0,0 +1,12 @@
+public class Warrior {
+        String helm; 
+        String weapon; 
+        String shield; 
+        int strength;
+        int dexterity; 
+        
+        void makeWarrior() {
+           // int str = (int) (Math.random() * 19);
+            System.out.println("the warrior's strength is generated here?");
+        }
+    }
