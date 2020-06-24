fce107bc22853ffee14b43b2244b8ce3c2bb2fac
jcostopulos <costopulos@gmail.com>
2020-06-23 17:38:36 -0700

    cleaned up code, coded damage generator method for spells and weapons.

--- Wizard.java
+++ Wizard.java
@@ -1,4 +1,8 @@
 public class Wizard {
+
+    // properties (variables that will be used for each instance of this class ie.
+    // for each wizard.
+
     String hat; 
     String wand;
     String spell;
@@ -6,38 +10,31 @@ public class Wizard {
     int wisdom;
     int spellDmg;
 
-    // This constructor holds a method for randomizing and
-    // assigning a spell called from the WizardSpells file.
-    void randomizeDmg() {
-        //variable to hold damage that the spell will do
-        this.spellDmg = damageGenerator.wizDmg;
-    }
+    // TODO: 6/20/20 what is the difference between public Wizard and void makeWizard?
+    //  In that they are both running code but how are they acting differently??
+            // --- Answer: one is a constructor and one is a method.
+            // --- review how they are scoped.
 
-    // TODO: 6/20/20 what is the difference between public Wizard and void makeWizard?? In that they
-    // TODO: 6/20/20 are both running code but how are they acting differently?? 
     // constructor (method that gets called when you create the object). Build the Wizard here..
     public Wizard() {
-             //variable to hold the spell.
+             //gets a spell from the spell book.
             this.spell = WizardSpells.GetRandomSpell();
-             //variable to hold
-            this.spellDmg = damageGenerator.wizDmg;
+             //gets spell damage from damageGenerator.
+            this.spellDmg = damageGenerator.spellDamage();
         System.out.println("I am born");
-        System.out.println("my spellbook holds this spell...");
-        //variable to hold the spell.
-        //spell = WizardSpells.spellBook[WizardSpells.spellRandomizer];
-        System.out.println(spell);
-        //Damage generator.
-        randomizeDmg();
-        System.out.println("The damage this spell will do is " + spellDmg);
+        printStatus();
+        String printSpellDmg = "The damage this spell will do is " + spellDmg;
+        System.out.println(printSpellDmg);
         }
 
-        // methods! (a function that exists inside of a class). Methods manipulate the object..
-        // void makeWizard() {}
+    // methods! (a function that exists inside of a class). Methods manipulate the object..
+    // void makeWizard() {}
+    public void printStatus() {
+        System.out.println("my spellbook holds this spell...");
+        System.out.println(spell);
+    }
 
-        void castSpell() {
+    public void castSpell() {
             System.out.println("I shall cast " + spell);
-            System.out.println(" ");
-
-            // System.out.println("spell complete");
         }
 }
--- damageGenerator.java
+++ damageGenerator.java
@@ -1,12 +1,19 @@
-
-
-
-
+import java.util.Random;
 
 public class damageGenerator {
     // TODO: 6/19/20 make dmgRandomizer method run on its own
     //  PRIOR to calling it so it will be a unique number each time.
-   //public static damageGenerator wiz = new damageGenerator() {};
+    //public static damageGenerator wiz = new damageGenerator() {};
 
-    public static int wizDmg = (int) (Math.random() * 30);
+    public static int spellDamage() {
+        Random spellRand = new Random();
+        int dmgDoneBySpell = spellRand.nextInt(30);
+        return dmgDoneBySpell;
+    }
+
+    public static int weaponDamage() {
+        Random weaponRand = new Random();
+        int dmgDoneByWeapon = weaponRand.nextInt(55);
+        return dmgDoneByWeapon;
+    }
 }
